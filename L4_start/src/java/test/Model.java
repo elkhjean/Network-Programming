d65ceeafa4d9d377aea6de9c618f955d71c5f6d8
Robert Furuvald <rfu@kth.se>
2023-11-24 09:55:58 +0100

    L4 start

---
+++ L4_start/src/java/test/Model.java
@@ -0,0 +1,17 @@
+package test;
+
+
+public class Model {
+    private String username;
+    
+        public Model(){
+        }
+        
+        public String getUsername(){
+            return this.username;
+        }
+        
+        public void setUsername(String username){
+            this.username = username;
+        }
+}
