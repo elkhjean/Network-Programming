ce26df0b9e18a85c4b5d98b018829dc98e613809
elkhjean <sean.elkhoury@gmail.com>
2023-12-01 15:36:40 +0100

    quizz

--- l4/src/main/java/com/idunno/QuizzModel.java
+++ l4/src/main/java/com/idunno/QuizzModel.java
@@ -1,5 +1,69 @@
 package com.idunno;
 
-public class QuizzModel {
+import java.util.ArrayList;
+import java.util.Random;
 
+public class QuizzModel {
+    private String userId;
+    private String quizzId;
+    private ArrayList<Integer> questionIds;
+    private Question currentQuestion;
+
+    public QuizzModel(String userId) {
+        this.userId = userId;
+    }
+
+    public int pickRandomQuestionId() {
+        Random ran = new Random();
+        int i = ran.nextInt(questionIds.size());
+        int qId = questionIds.get(i);
+        questionIds.remove(i);
+        return qId;
+    }
+
+    public void setQuizzId(String quizId) {
+        this.quizzId = quizId;
+    }
+
+    public boolean checkGuess(String guess) {
+        if (currentQuestion.equals(guess)) {
+            return true;
+        }
+        return false;
+    }
+
+    public Question getQuestion() {
+        return this.currentQuestion;
+    }
+
+    class Question {
+        private String[] options;
+        private String answer;
+        private String questionText;
+        private int questionId;
+
+        public Question(String options, String answer, String questionText, String questionId) {
+            this.questionId = Integer.parseInt(questionId);
+            this.questionText = questionText;
+            this.answer = answer.replace("/", "");
+            this.options = options.split("/");
+        }
+
+        public String[] getOptions() {
+            return options;
+        }
+
+        public String getAnswer() {
+            return answer;
+        }
+
+        public String getQuestionText() {
+            return questionText;
+        }
+
+        public int getQuestionId() {
+            return questionId;
+        }
+
+    }
 }
