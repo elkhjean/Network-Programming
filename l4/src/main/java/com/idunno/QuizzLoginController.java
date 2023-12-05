059501223773e8a454dda02a315ae39ea36fb079
elkhjean <sean.elkhoury@gmail.com>
2023-12-05 11:12:20 +0100

    login page

--- l4/src/main/java/com/idunno/QuizzLoginController.java
+++ l4/src/main/java/com/idunno/QuizzLoginController.java
@@ -2,23 +2,37 @@ package com.idunno;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-@WebServlet(name = "DBServlet", urlPatterns = { "/DBServlet" })
 public class QuizzLoginController extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
+            throws IOException, ServletException {
         response.setContentType("text/html");
+
+        HttpSession session = request.getSession(true);
+        RequestDispatcher rd = request.getRequestDispatcher("quizzLoginView.jsp");
+        rd.forward(request, response);
+
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        HttpSession session = request.getSession();
         PrintWriter out = response.getWriter();
         out.println("Connecting to DB.");
         try {
@@ -26,15 +40,24 @@ public class QuizzLoginController extends HttpServlet {
             Context envContext = (Context) initContext.lookup("java:/comp/env");
             DataSource ds = (DataSource) envContext.lookup("jdbc/derby");
             Connection conn = ds.getConnection();
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from users");
-            while (rs.next()) {
-                out.print(rs.getString("username") + "<br>");
-                out.println(rs.getString("password"));
+
+            PreparedStatement pstmt = conn.prepareStatement("SELECT ID FROM users WHERE username = ? AND password = ?");
+            pstmt.setString(1, request.getParameter("username"));
+            pstmt.setString(2, request.getParameter("password"));
+            ResultSet rs = pstmt.executeQuery();
+            if (!rs.next()) {
+                request.setAttribute("loginFailed", true);
+                RequestDispatcher rd = request.getRequestDispatcher("quizzLoginView.jsp");
+                rd.forward(request, response);
+            } else {
+                session.setAttribute("model", new QuizzModel(rs.getString("ID")));
+                RequestDispatcher rd = request.getRequestDispatcher("/ChooseQuizzController");
+                rd.forward(request, response);
             }
         } catch (Exception e) {
-            out.println(e.getMessage());
+            e.printStackTrace();
+            out.println("Database connection problem.");
         }
-        out.println("Finished.");
+
     }
 }
